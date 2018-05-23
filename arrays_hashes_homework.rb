ef2b561a5184e51afe98f34c398dc63c70bf367e
Adams1991 <adams121@hotmail.co.uk>
2018-05-23 18:46:11 +0100

    exercise B apart from 4 & 10 (need more work)

--- arrays_hashes_homework.rb
+++ arrays_hashes_homework.rb
@@ -2,58 +2,58 @@
 
 # Given the following data structure:
 
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-#
-# # Complete these tasks:
-#
-# #1. Add `"Edinburgh Waverley"` to the end of the array
-#
-p stops.push("Edinburgh Waverley")
-
-# #
-# # # #2. Add `"Glasgow Queen St"` to the start of the array
-# #
-p stops.unshift("Glasgow Queen St")
-
-# #
-# # # #3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-# #
-p stops.insert(4, "Polmont")
-
-
-# # #4. Work out the index position of `"Linlithgow"`
-
-p stops.index("Linlithgow") # answer will include above changes if not commented out or put in function
-
-# # #5. Remove `"Livingston"` from the array using its name
-# #
-p stops.delete("Livingston")
-p stops
-
-
-# #6. Delete `"Cumbernauld"` from the array by index
-#
-p stops.delete_at(1)
-
-
-# #7. How many stops there are in the array?
-
-p stops.length() # answer will include above changes if not commented out or put in function
-# #8. How many ways can we return `"Falkirk High"` from the array?
-
-p stops[2]
-p stops.fetch(2)
-p stops.delete_at(2)
-
-# #9. Reverse the positions of the stops in the array
-
-p stops.reverse()
-
-# #10. Print out all the stops using a for loop
 # stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-for stop in stops
-  p stop
-end
+# #
+# # # Complete these tasks:
+# #
+# # #1. Add `"Edinburgh Waverley"` to the end of the array
+# #
+# p stops.push("Edinburgh Waverley")
+#
+# # #
+# # # # #2. Add `"Glasgow Queen St"` to the start of the array
+# # #
+# p stops.unshift("Glasgow Queen St")
+#
+# # #
+# # # # #3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# # #
+# p stops.insert(4, "Polmont")
+#
+#
+# # # #4. Work out the index position of `"Linlithgow"`
+#
+# p stops.index("Linlithgow") # answer will include above changes if not commented out or put in function
+#
+# # # #5. Remove `"Livingston"` from the array using its name
+# # #
+# p stops.delete("Livingston")
+# p stops
+#
+#
+# # #6. Delete `"Cumbernauld"` from the array by index
+# #
+# p stops.delete_at(1)
+#
+#
+# # #7. How many stops there are in the array?
+#
+# p stops.length() # answer will include above changes if not commented out or put in function
+# # #8. How many ways can we return `"Falkirk High"` from the array?
+#
+# p stops[2]
+# p stops.fetch(2)
+# p stops.delete_at(2)
+#
+# # #9. Reverse the positions of the stops in the array
+#
+# p stops.reverse()
+#
+# # #10. Print out all the stops using a for loop
+# # stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# for stop in stops
+#   p stop
+# end
 
 
 #
@@ -61,74 +61,106 @@ end
 #
 # # Given the following data structure:
 #
-# users = {
-#   "Jonathan" => {
-#     :twitter => "jonnyt",
-#     :lottery_numbers => [6, 12, 49, 33, 45, 20],
-#     :home_town => "Stirling",
-#     :pets => [
-#     {
-#       :name => "fluffy",
-#       :species => "cat"
-#     },
-#     {
-#       :name => "fido",
-#       :species => "dog"
-#     },
-#     {
-#       :name => "spike",
-#       :species => "dog"
-#     }
-#   ]
-#   },
-#   "Erik" => {
-#     :twitter => "eriksf",
-#     :lottery_numbers => [18, 34, 8, 11, 24],
-#     :home_town => "Linlithgow",
-#     :pets => [
-#     {
-#       :name => "nemo",
-#       :species => "fish"
-#     },
-#     {
-#       :name => "kevin",
-#       :species => "fish"
-#     },
-#     {
-#       :name => "spike",
-#       :species => "dog"
-#     },
-#     {
-#       :name => "rupert",
-#       :species => "parrot"
-#     }
-#   ]
-#   },
-#   "Avril" => {
-#     :twitter => "bridgpally",
-#     :lottery_numbers => [12, 14, 33, 38, 9, 25],
-#     :home_town => "Dunbar",
-#     :pets => [
-#       {
-#         :name => "monty",
-#         :species => "snake"
-#       }
-#     ]
-#   }
-# }
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
 #
 # # Complete these tasks:
 #
 # #1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# #2. Get Erik's hometown
-# #3. Get the array of Erik's lottery numbers
+
+p users["Jonathan"][:home_town]
+#
+# # #2. Get Erik's hometown
+#
+p users["Erik"][:home_town]
+#
+# # #3. Get the array of Erik's lottery numbers
+#
+p users["Erik"][:lottery_numbers]
+
 # #4. Get the type of Avril's pet Monty
+
+# p users["Avril"][:pets][:species] NEEDS FIXED
+
 # #5. Get the smallest of Erik's lottery numbers
-# #6. Return an array of Avril's lottery numbers that are even
+
+p users["Erik"][:lottery_numbers].min
+#
+# # #6. Return an array of Avril's lottery numbers that are even
+#
+p users["Avril"][:lottery_numbers].select(&:even?)
+
 # #7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+p users["Erik"][:lottery_numbers] << 7
+
 # #8. Change Erik's hometown to Edinburgh
+
+p users["Erik"][:home_town] =  "Edinburgh"
+p users["Erik"]
+
 # #9. Add a pet dog to Erik called "Fluffy"
+
+users["Erik"][:pets] <<  {:name => "Fluffy", :species => "dog"}
+p users["Erik"][:pets]
+
+
 # #10. Add another person to the users hash
+
+
 #
 #
 # # Exercise C
